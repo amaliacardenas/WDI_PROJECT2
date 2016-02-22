65a109635ac210ebe89805f7aa5e3ff202dc9df5
amaliacardenas <amaliacardenas@gmail.com>
2016-02-22 19:05:27 +0000

    started to add some forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,53 @@
-#INDEX
-
-get '/users' do
+# INDEX
+get "/users" do
+  authorize!
   @users = User.all
   erb :"users/index"
 end
 
-#NEW
+# NEW
+get "/users/new" do
+  authorize!
+  @user = User.new
+  erb :"users/new"
+end
 
-#CREATE
+# CREATE
+post "/users" do
+  authorize!
+  @user = User.new(params[:user])
+  if @user.save
+    redirect "/users"
+  else
+    erb :"users/new"
+  end
+end
 
-#SHOW
+# SHOW
+get "/users/:id" do
+  authorize!
+  @user = User.find(params[:id])
+  if @user
+    erb :"users/show"
+  else
+    redirect "/users"
+  end
+end
 
-#EDIT
+# EDIT
+get "/users/:id/edit" do
+  authorize!
+  @user = User.find(params[:id])
+  erb :"users/edit"
+end
 
-#UPDATE
-
-
-#DELETE
+# UPDATE
+put '/users/:id' do
+  authorize!
+  @user = User.find(params[:id])
+  if @user.update(params[:user])
+    redirect "/users/#{@user.id}"
+  else
+    erb :"users/show"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222172330) do
+ActiveRecord::Schema.define(version: 20160222184513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160222172330) do
     t.string "username"
     t.string "password"
     t.string "image"
+    t.string "email"
   end
 
 end
