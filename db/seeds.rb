f64a724391b496382239d4f8481c5c999b05b8c6
amaliacardenas <amaliacardenas@gmail.com>
2016-02-23 18:04:17 +0000

    created the category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require_relative "../app/models/tutorial.rb"
+require_relative "../app/models/category.rb"
 
 Tutorial.destroy_all
+Category.destroy_all
+
 [
   {
     title: "Michael Hartl's Rails Tutorial",
@@ -9,7 +12,7 @@ Tutorial.destroy_all
     description: "Michael Hartl's epic tutorial covers everything from installing Rails to using Github, deploying on Heroku and writing tests. It takes awhile to get through but once you've finished you'll be set to start writing your own apps. - @kateray",
     difficulty: "Beginer",
     vote: 5,
-    category_id: "Rails"
+    category_id: 1
   },
   {
     title: "iOS Programming: The Big Nerd Ranch Guide",
@@ -18,7 +21,7 @@ Tutorial.destroy_all
     description: "Big Nerd Ranch is one of the leading publishers of mobile app dev books. This book is extremely in-depth and teaches you a lot about Objective C and some theory around iOS dev. - @kateray",
     difficulty: "Beginner",
     vote: 5,
-    category_id: "iOS"
+    category_id: "2"
   },
   {
     title: "Make a ToDo app",
@@ -27,7 +30,7 @@ Tutorial.destroy_all
     description: "Great simple example Backbone app. Has a model, view, and collection, and persists data locally using LocalStorage. - @kateray",
     difficulty: "Beginner",
     vote: 3,
-    category_id: "Backbone"
+    category_id: "3"
   },
   {
     title: "The Node Beginner Book",
@@ -36,7 +39,7 @@ Tutorial.destroy_all
     description: "A very comprehensive introduction to Node and Javascript. - @kateray",
     difficulty: "Beginner",
     vote: 3,
-    category_id: "Node"
+    category_id: "4"
   },
  {
    title: "Real Python",
@@ -45,8 +48,49 @@ Tutorial.destroy_all
    description: "Real Python teaches Python and web development from the ground up, everything from the basic primitives to web scraping to data visualization and all things web development!",
    difficulty: "Beginer",
    vote: 4,
-   category_id: "Python"
+   category_id: "5"
  }
 ].each do |tutorial|
   Tutorial.create!(tutorial)
+end
+[
+  { 
+    title: "Rails",
+    icon: "https://cdn1.iconfinder.com/data/icons/simple-icons/2048/rails-2048-black.png"
+  },
+  { 
+    title: "iOS",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "Backbone",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "Node",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "Python",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "Javascript",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "Ruby",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "HTML",
+    icon: "PICTURE HERE"
+  },
+  { 
+    title: "CSS",
+    icon: "PICTURE HERE"
+  }
+
+].each do |category|
+  Category.create!(category)
 end
